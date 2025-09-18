f2afe20bc4aba9cb12eaf90fa3e122f6d931545d
Mario Hernandez <mhmario963@gmail.com>
2025-09-18 00:15:52 -0400

    create multi binary entry points

--- runner.sh
+++ runner.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-BINARY_NAME="network"
+if [ "$#" -ne 1 ]; then
+	echo "usage: $0 {updclient|updserver}"
+	exit 1
+fi
+
+
+PROJECT_NAME="$1"
+
+case "$PROJECT_NAME" in	
+	updclient)
+		;;
+	updserver)
+		;;
+	*)
+		echo "usage: $0 {updclient|updserver}"
+		exit 1
+		;;
+esac
+
+
+BINARY_NAME="$PROJECT_NAME.exe"
 
 if [ -f "$BINARY_NAME" ]; then
 	echo "removing old binary"
 	rm "$BINARY_NAME"
 fi
 
-go build -o "$BINARY_NAME" .
+go build -o "$BINARY_NAME" "/Users/mariohernandez/development/network-apps/cmd/$PROJECT_NAME"
 
 if [ $? -eq 0 ]; then
 	./"$BINARY_NAME"
