6422acaa07785cb4d82da2ec543fbfa20619ddcd
Mario Hernandez <mhmario963@gmail.com>
2025-09-22 23:24:16 -0400

    handle reading user input until EOF in client

--- cmd/udpclient/main.go
+++ cmd/udpclient/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
+	"sync"
 )
 
 const (
@@ -13,15 +18,20 @@ const (
 )
 
 func main() {
+	//	sigChan := make(chan os.Signal)
+	//	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	//	quitChan := make(chan struct{})
+
 	clientAddr := "192.168.1.112:7070"
 	udpClientAddr, err := net.ResolveUDPAddr(udpNetwork, clientAddr)
 	if err != nil {
-		log.Fatalf("resolve udp client address=%v: %w", clientAddr, err)
+		log.Fatalf("resolve udp client address=%v: %v", clientAddr, err)
 	}
 
 	conn, err := net.ListenUDP(udpNetwork, udpClientAddr)
 	if err != nil {
-		log.Fatalf("listen to UDP network: %w", err)
+		log.Fatalf("listen to UDP network: %v", err)
 	}
 	defer conn.Close()
 
@@ -30,17 +40,85 @@ func main() {
 	srvrAddr := "192.168.1.112:8080"
 	udpSrvrAddr, err := net.ResolveUDPAddr(udpNetwork, srvrAddr)
 	if err != nil {
-		log.Fatalf("resolve udp server address: %w", err)
+		log.Fatalf("resolve udp server address=%s: %v", srvrAddr, err)
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("Enter message to send to server: ")
-		msg, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatalf("read string %w", err)
+	pipelineCtx, pipelineCancel := context.WithCancelCause(context.Background())
+	defer pipelineCancel(nil)
+
+	errChan := make(chan error)
+	var wg sync.WaitGroup
+
+	msgChan := make(chan string)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		readUserInput(pipelineCtx, msgChan, errChan)
+	}()
+
+	wg.Add(1)
+	go func() {
+		writeToServer(pipelineCtx, msgChan, conn, udpSrvrAddr, errChan)
+	}()
+
+	go func() {
+		defer close(errChan)
+
+		wg.Wait()
+		fmt.Println("good bye!")
+	}()
+
+	errPipelineCanceled := fmt.Errorf("pipeline canceled: %w", context.Canceled)
+	for err := range errChan {
+		if err == nil || (errors.Is(err, context.Canceled) && errors.Is(context.Cause(pipelineCtx), errPipelineCanceled)) {
+			continue
 		}
 
-		conn.WriteToUDP([]byte(msg), udpSrvrAddr)
+		fmt.Println("canceling pipeline due to error: %v", err)
+	}
+}
+
+func readUserInput(ctx context.Context, msgChan chan<- string, errChan chan<- error) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter message to send to server(EOF to exit program): ")
+		msg, err := reader.ReadString('\n')
+		if err != nil && errors.Is(err, io.EOF) {
+			fmt.Println("EOF reached.")
+			return
+		}
+		if err != nil {
+			errChan <- fmt.Errorf("reading user input: %w", err)
+			return
+		}
+
+		select {
+		case <-ctx.Done():
+			return
+		case msgChan <- strings.TrimSpace(msg):
+			// no-op
+		}
+
+	}
+}
+
+func writeToServer(ctx context.Context, msgChan <-chan string, conn *net.UDPConn, udpSrvrAddr *net.UDPAddr, errChan chan<- error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case msg, ok := <-msgChan:
+			if !ok {
+				fmt.Println("message channel closed")
+				return
+
+			}
+			_, err := conn.WriteToUDP([]byte(strings.TrimSpace(msg)), udpSrvrAddr)
+			if err != nil {
+				errChan <- fmt.Errorf("error writing to UDP server", err)
+				return
+			}
+		}
+
 	}
 }
